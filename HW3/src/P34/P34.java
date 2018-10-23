7727a92d6cf6cfbe11cd413d7cfe02cb9595c7a2
george0895 <george6464982@yahoo.com.tw>
2018-10-23 23:54:46 +0800

    first commit

---
+++ HW3/src/P34/P34.java
@@ -0,0 +1,22 @@
+package P34;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P34 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+			System.out.println("搶輸入一個整數");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			String str =br.readLine();
+			
+			int num = Integer.parseInt(str);
+			
+			System.out.println("您輸入的數字是"+num);
+	}
+
+}
---
+++ HW3/src/P5/P5.java
@@ -0,0 +1,15 @@
+package P5;
+
+public class P5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1= new Car();
+	}
+
+}
+
+class Car{
+	//car Ãþ§O
+}
